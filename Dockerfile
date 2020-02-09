5900434014f4955f286247153743f3f8ed089e48
Simon DENEL <sdenel@smartwavesa.com>
2020-02-09 15:49:39 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM alpine:latest
+
+COPY setup.sh /opt/setup.sh
+RUN chmod +x /opt/setup.sh && /opt/setup.sh
+
+ENV CHROME_BIN=/usr/bin/chromium-browser \
+    CHROME_PATH=/usr/lib/chromium/
---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+echo "# Installing common tools"
+apk add curl git npm p7zip util-linux jq bash binutils
+
+echo "# Installing Chromium package."
+apk upgrade && apk add --no-cache \
+    chromium \
+    freetype \
+    harfbuzz \
+    libstdc++ \
+    nss \
+    ttf-freefont
+
+echo "# Getting @angular/cli"
+npm install -g @angular/cli
+
+echo "# Getting brotli"
+apk add brotli
+
+
+echo "# Getting kubectl"
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/bin/kubectl
+
+echo "# Getting aws cli"
+apk add py-pip
+pip install --upgrade pip
+pip install awscli
+
+
+echo "# Cleaning cache files"
+rm -rf /var/cache/* \
+mkdir /var/cache/apk
+
+
